36634e12f47e87477852bfbb35e202dcb5632bbc
Maycon Douglas <85197511+WzK000@users.noreply.github.com>
2024-06-19 03:05:13 +0000

    Teste de commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,9 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Maycon Douglas",
+  title: "Product Designer & Front End Developer",
+  email: "oi@maycondouglas.com",
   gitHub: "microsoft",
   instagram: "microsoft",
   linkedIn: "satyanadella",
